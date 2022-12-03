57701503ddbe19c04104d8b3e7ab0504020b2a58
cfoster5 <cfoster3204@gmail.com>
2022-12-03 07:56:51 -0600

    Initial version of animated graph for genres

---
+++ app/AnimatedComponent.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const draw = {
+  hidden: { width: 0, opacity: 0 },
+  visible: ({ position, width }) => {
+    const delay = 1 + position * 0.5;
+    return {
+      width: `${width * 100}%`,
+      opacity: 1,
+      transition: {
+        width: { delay, type: "spring", duration: 1, bounce: 0 },
+        opacity: { delay, duration: 0.01 },
+      },
+    };
+  },
+};
+
+const Bar = ({ color, position, width, text }) => (
+  <motion.div
+    className={`h-10 whitespace-nowrap rounded-r-lg bg-[${color}]`}
+    variants={draw}
+    initial="hidden"
+    animate="visible"
+    custom={{ position, width }}
+  >
+    <p className="ml-4 flex h-full items-center capitalize">{text}</p>
+  </motion.div>
+);
+
+const composeArray = (map) =>
+  [...map].map(([name, value]) => ({ name, value }));
+
+export const AnimatedComponent = ({ movies }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 3500);
+  }, []);
+
+  const genres = movies.map((movie) => movie.movie.genres).flat();
+
+  const uniqueGenres = [...new Set(genres)];
+
+  // https://stackoverflow.com/a/57028486/5648619
+  const genreMap = genres.reduce(
+    (acc, e) => acc.set(e, (acc.get(e) || 0) + 1),
+    new Map()
+  );
+
+  const genreDict = composeArray(genreMap);
+  const sortedGenreDict = genreDict.sort((a, b) => b.value - a.value);
+
+  const colors = ["#845EC2", "#D65DB1", "#FF6F91", "#FF9671", "#FFC75F"];
+
+  return (
+    <AnimatePresence mode="wait">
+      {!isVisible ? (
+        <motion.p
+          key="first"
+          className="mb-2 flex-1 self-center text-center text-2xl text-[#F9F871]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          In 2022, you explored {uniqueGenres.length} different genres
+        </motion.p>
+      ) : (
+        //     <motion.p
+        //       className="mb-2 flex-1 self-center text-2xl text-[#F9F871]"
+        //       initial={{ opacity: 0 }}
+        //       animate={{ opacity: 1 }}
+        //       exit={{ opacity: 0 }}
+        //     >
+        //       Your top genres
+        //     </motion.p>
+        //   )}
+        // </AnimatePresence>
+        // <div className="flex-1">
+        //   <Bar color="#845EC2" position={0} width={1} text="Action" />
+        //   <Bar color="#D65DB1" position={1} width={0.75} text="Adventure" />
+        //   <Bar color="#FF6F91" position={2} width={0.5} text="Science Fiction" />
+        //   <Bar color="#FF9671" position={3} width={0.35} text="Thriller" />
+        //   <Bar color="#FFC75F" position={4} width={0.2} text="Superhero" />
+        // </div>
+        <>
+          <motion.p
+            key="second"
+            className="mb-2 flex-1 self-center text-2xl text-[#F9F871]"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            Your top genres
+          </motion.p>
+          <div className="flex-1">
+            {sortedGenreDict.slice(0, 5).map((genre, i) => (
+              <Bar
+                key={i}
+                color={colors[i]}
+                position={i}
+                width={genre.value / sortedGenreDict[0].value}
+                text={genre.name}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,67 @@
-import Image from 'next/image'
+import Image from "next/image";
 // import styles from './page.module.css'
 
-export default function Home() {
+// Import your Client Component
+import Link from "next/link";
+import { History } from "../types";
+import { AnimatedComponent } from "./AnimatedComponent";
+
+async function getMovieHistories(): Promise<History[]> {
+  const res = await fetch(
+    "https://api.trakt.tv/users/cfoster5/history/movies?start_at=2022-01-01T00:00:00.000Z&extended=full",
+    {
+      headers: {
+        "trakt-api-key": `${process.env.TRAKT_ID}`,
+      },
+    }
+  );
+  const history = await res.json();
+  return history;
+}
+
+async function getMovies(movieHistories: History[]) {
+  const movies = await Promise.all(
+    movieHistories.map(async (movieHistory) => await getMovie(movieHistory))
+  );
+  return movies;
+}
+
+async function getMovie(movieHistory: History) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieHistory.movie.ids.tmdb}?language=en-US&api_key=${process.env.TMDB_KEY}&append_to_response=credits`
+  );
+  return { ...movieHistory, tmdb: await res.json() };
+}
+
+export default async function Page() {
+  const movieHistory = await getMovieHistories();
+  const movies = await getMovies(movieHistory);
+
   return (
     <div>
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
+      <main className="text-slate-50">
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl">Watch Wrapped</h1>
+          <Link href="/api/auth/signin">Sign In</Link>
+        </div>
+        {/* <div className="mt-8 flex flex-wrap justify-center gap-4">
+          {movies.map((movie) => (
+            <Image
+              className="rounded-lg border-2 border-slate-800"
+              key={movie.tmdb.poster_path}
+              src={`https://image.tmdb.org/t/p/w500${movie.tmdb.poster_path}`}
+              width={342}
+              height={342 / 1.78}
+              alt=""
+            />
+            // <p>{JSON.stringify(movie.tmdb.credits.cast)}</p>
+          ))}
+        </div> */}
+        {/* <p className="mt-8 flex justify-center">{JSON.stringify(movies)}</p> */}
+        <div className="mt-8 flex flex-col">
+          <AnimatedComponent movies={movies} />
+        </div>
       </main>
     </div>
-  )
+  );
 }
