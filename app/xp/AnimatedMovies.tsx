041988eff7cce56797e00ccbec534b1ed2324b42
cfoster5 <cfoster3204@gmail.com>
2022-12-04 15:59:22 -0600

    Initial commit for animated movie poster

---
+++ app/xp/AnimatedMovies.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { History } from "../../types";
+
+const cardVariants = {
+  initial: {
+    opacity: 0,
+    x: 0,
+    rotate: -10,
+  },
+  final: (custom) => {
+    const delay = custom * 0.35;
+    return {
+      opacity: 1,
+      // x: 900 - 100 * custom,
+      // x: `calc(100% - 100px * ${custom})`,
+      x: `calc(100%)`,
+      rotate: 0,
+      transition: {
+        x: { delay, type: "spring", bounce: 0.4, duration: 0.8 },
+        rotate: { delay, type: "spring", bounce: 0.4, duration: 0.8 },
+        opacity: { delay, type: "spring", bounce: 0.4, duration: 0.8 },
+      },
+    };
+  },
+};
+
+const AnimatedPoster = ({ movie, position }) => (
+  <motion.img
+    className="absolute"
+    initial="initial"
+    animate="final"
+    custom={position}
+    variants={cardVariants}
+    src={`https://image.tmdb.org/t/p/w500${movie.tmdb.poster_path}`}
+  />
+);
+
+export const AnimatedMovies = ({ movies }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 3500);
+  }, []);
+
+  const unique = [...new Set(movies.map((item) => item.movie.ids.trakt))];
+
+  return (
+    <AnimatePresence mode="wait">
+      {!isVisible ? (
+        <motion.p
+          key="first"
+          className="mb-2 flex-1 self-center text-center text-2xl text-[#F9F871]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          This year, you watched {unique.length} movies
+        </motion.p>
+      ) : (
+        <>
+          <motion.p
+            key="second"
+            className="mb-2 flex-1 self-center text-2xl text-[#F9F871]"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            Your movies
+          </motion.p>
+          <div className="relative flex">
+            <div className="flex-1">
+              {[...movies].reverse().map((movie, i) => (
+                <AnimatedPoster key={movie.id} movie={movie} position={i} />
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
---
+++ app/xp/page.tsx
@@ -0,0 +1,34 @@
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+// import styles from './page.module.css'
+
+// Import your Client Component
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { History } from "../../types";
+import { getMovieHistories } from "../api/getMovieHistories";
+import { AnimatedMovies } from "./AnimatedMovies";
+
+async function getMovies(movieHistories: History[]) {
+  const movies = await Promise.all(
+    movieHistories.map(async (movieHistory) => await getMovie(movieHistory))
+  );
+  return movies;
+}
+
+async function getMovie(movieHistory: History) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieHistory.movie.ids.tmdb}?language=en-US&api_key=${process.env.TMDB_KEY}&append_to_response=credits`
+  );
+  return { ...movieHistory, tmdb: await res.json() };
+}
+
+export default async function Page() {
+  const movieHistory = await getMovieHistories();
+  const movies = await getMovies(movieHistory);
+  return (
+    <div className="mt-8 flex flex-col">
+      <AnimatedMovies movies={movies} />
+    </div>
+  );
+}
