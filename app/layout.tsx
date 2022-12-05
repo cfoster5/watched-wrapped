2ed3eec84ff07150704422f8c0ce22d2fc481fad
cfoster5 <cfoster3204@gmail.com>
2022-12-05 07:12:46 -0600

    Updated shared header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Inter } from "@next/font/google";
 
@@ -20,7 +21,18 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="flex flex-row items-center justify-between">
+          <h1 className="text-xl">Watch Wrapped</h1>
+          <Link href="/">Experience</Link>
+          {/* <div className="grid grid-cols-3 justify-center">
+            <h1 className="col-start-2 text-4xl">Watch Wrapped</h1>
+            <Link href="/xp" className="my-auto ml-auto">
+              Experience
+            </Link>
+          </div> */}
+        </div>
+      </body>
     </html>
   );
 }
